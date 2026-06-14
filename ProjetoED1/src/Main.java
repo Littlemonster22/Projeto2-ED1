782b02c38b986df865b2ca46b25c531d3d137871
Matheus Santos de Jesus <matheussantosjesus126@gmail.com>
2026-06-13 21:59:53 -0300

    organizar o código

--- ProjetoED1/src/Main.java
+++ ProjetoED1/src/Main.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 import static Objetos.leituraVetor.exibirListaFilmes;
 import static Objetos.leituraVetor.lerArquivoCSV;
 
-
 public class Main {
     public static void main(String[] args) {
         int opcao;
